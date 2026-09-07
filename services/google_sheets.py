f31702659de07de232613e31c11b6ab3c74bd141
hhichriChamp <haikel.amazon@gmail.com>
2026-09-06 21:06:10 -0400

    adding secrets toml

--- services/google_sheets.py
+++ services/google_sheets.py
@@ -2,20 +2,6 @@ import gspread
 import streamlit as st
 
 
-# def get_google_sheet():
-#     """
-#     Connect to the Google Spreadsheet.
-#     """
-
-#     credentials = dict(st.secrets["gcp_service_account"])
-
-#     gc = gspread.service_account_from_dict(credentials)
-
-#     spreadsheet = gc.open("Tutor Tracking System")
-
-#     return spreadsheet
-
-
 def get_google_sheet():
 
     credentials = dict(st.secrets["gcp_service_account"])
@@ -23,21 +9,16 @@ def get_google_sheet():
     gc = gspread.service_account_from_dict(credentials)
 
     try:
-        # spreadsheet = gc.open("Tutor Tracking System")
-        spreadsheet = gc.open_by_key("1VrbV0NHmP5e2nfscIqhNHTP9cONmBwf3v9rCMNndlkY")
+        spreadsheet = gc.open_by_key(
+            "1VrbV0NHmP5e2nfscIqhNHTP9cONmBwf3v9rCMNndlkY"
+        )
+
+        return spreadsheet
 
     except Exception as e:
         st.error(f"Could not open spreadsheet: {e}")
         raise
 
-# def read_classes_from_sheet():
-#     spreadsheet = get_google_sheet()
-
-#     worksheet = spreadsheet.worksheet("Classes")
-
-#     records = worksheet.get_all_records()
-
-#     return records
 
 def read_classes_from_sheet():
 
